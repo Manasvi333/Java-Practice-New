5e64e3625e7a11daebc5b7997c1ffba37c579d29
Manasvi333 <manasvi032006@gmail.com>
2026-07-07 13:37:41 +0530

    Rectangle pattern

--- Patterns/Rectangle.java
+++ Patterns/Rectangle.java
@@ -1,15 +1,15 @@
-package Patterns;
 import java.util.Scanner;
-public class Square {
+public class Rectangle {
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
-        System.out.println("Enter the number of rows and columns");
+        System.out.println("Enter the number of rows");
         int n=sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=5;j++){
                 System.out.print("@ ");
             }
             System.out.println();
         }
+    
 }
 }
